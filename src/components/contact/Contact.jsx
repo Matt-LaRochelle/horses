6dae4a918b025f5f1eff147a473d40706a96b843
matthew <larochelle.matthew@gmail.com>
2023-04-05 12:00:29 -0700

    contact form complete

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,17 +5,20 @@ function Contact() {
     return (
         <div className={styles.container}>
             <h1 className={styles.h1}>Contact Us</h1>
-            <form className={styles.container}>
-                <div className={styles.name}>
-                    <input type="text" placeholder="First Name" name="first_name" required></input>
-                    <input type="text" placeholder="Last Name" name="last_name" required></input>
-                </div>
-                <div className={styles.content}>
-                    <input type="email" placeholder="Email" name="email" required></input>
-                    <input type="text" placeholder="Subject" name="subject" required></input>
-                    <textarea rows="8" cols="80" placeholder="Message" className={styles.message} name="message" required></textarea>
-                    <button className={styles.btn} type="submit">Submit</button>
-                </div>
+            <form className={styles.contact}>
+                <label>First Name</label>
+                <input className={styles.input} type="text" name="first_name" required></input>
+                <hr className={styles.hr}></hr>
+                <label>Last Name</label>
+                <input className={styles.input} type="text" name="last_name" required></input>
+                <hr className={styles.hr}></hr>
+                <label>Email</label>
+                <input className={styles.input} type="email" name="email" required></input>
+                <hr className={styles.hr}></hr>
+                <label>Message</label>
+                <textarea className={styles.message} rows="8" cols="40" name="message" required></textarea>
+                <hr className={styles.hr}></hr>
+                <button className={styles.btn} type="submit">Submit</button>
             </form>
         </div>
     );
