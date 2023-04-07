22a3e66626043e0397c42e12705e7c04d9c336a1
matthew <larochelle.matthew@gmail.com>
2023-04-07 16:50:28 -0700

    first attempt at the game

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './Game.module.css';
 
 function Game() {
+    const [jump, setJump] = useState(null);
+
+    function trexjump() {
+        setJump(styles.jump);
+        setTimeout(function () {
+            setJump(null);
+        }, 300);
+    }
+
     return (
         <div className={styles.game}>
-            <div className={styles.dino}></div>
+            <div className={  `head ${styles.dino} ${jump}`  }
+                 onKeyDown={(function (event) {
+                trexjump();
+                })}>
 
+            </div>
+            <div className={styles.cactus}></div>
         </div>
     );
 }
