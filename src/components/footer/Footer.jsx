a100b9d6f5c7272e0e4314cd335952e9f9cd62b2
matthew <larochelle.matthew@gmail.com>
2023-04-06 23:55:23 -0700

    adjust styles for phone

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,20 +6,10 @@ import l3 from '../images/l3.png';
 function Footer() {
     return (
         <footer className={styles.footer}>
-            <img className={styles.img} src={l1} />
-            <div className={styles.center}>
                 <p>15311 Baden Naylor Road</p>
                 <p>Brandywine, MD, 20613</p>
                 <p>windmill.equestrian.2011@gmail.com</p>
                 <p>301 861-9333</p>
-            </div>
-            <img className={styles.img} src={l3} />
-            <div className={styles.small}>
-                <img className={styles.img1} src={l1} />
-                <img className={styles.img1} src={l3} />
-            </div>
-            
-
         </footer>
     );
 }
