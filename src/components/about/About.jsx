eb75afb4d763cfc6abbfcbdd2c2da9291ce1ad3c
matthew <larochelle.matthew@gmail.com>
2023-04-06 23:43:44 -0700

    update about styles, images, grids

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import styles from './About.module.css';
-import h4 from '../images/h4.jpg';
+
 import cat from '../images/cat.png';
+import h2 from '../images/h2.jpg';
 import h3 from '../images/h3.jpg';
+import h4 from '../images/h4.jpg';
+import h6 from '../images/h6.jpg';
 
 function About() {
     return (
@@ -24,9 +27,11 @@ function About() {
 
             <h3 className={styles.h3}>Our Facility</h3>
             <div className={styles.grid}>
-                <p className={styles.p}>Nestled on 15 acres in Southern Maryland, Windmill Equestrian also serves as our family farm. Owned by Jon and Shoshana Haag, the family farm is home to horses, geese, chickens, and a very friendly barn cat named Cole.  Our outdoor arena has lights, and evening lessons are available. </p>
+                <p className={styles.pgrid}>Nestled on 15 acres in Southern Maryland, Windmill Equestrian also serves as our family farm. Owned by Jon and Shoshana Haag, the family farm is home to horses, geese, chickens, and a very friendly barn cat named Cole.  Our outdoor arena has lights, and evening lessons are available. </p>
                 <img src={h3} alt="horses in a barn" className={styles.horses} />
+                <img className={styles.cat} src={cat} alt="cat" />
             </div>
+            
 
             <h3 className={styles.h3}>Out Trainer</h3>
             <p className={styles.p}>Shoshana Haag has a passion for horses that started with riding at age three. She believes that horses should be for everyone and that riding should be safe, fun, and educational. She has been professionally involved in the equestrian world since 15, and has developed her riding and teaching skills in the 20 years since. Shoshana’s goal is to help students acquire the tools for success and build confidence, while learning to appreciate the art and the beauty of a relationship with  horses. By helping students step out of their comfort zone in a safe and positive environment, she allows students to develop skills, connect with horses, and ultimately progress. </p>
@@ -36,7 +41,7 @@ function About() {
                 <li className={styles.li}>Worked and trained under classical trainer Fernando Cardenas Sr.</li>
                 <li className={styles.li}>Competed in A Circuit jumping up to 3’9”</li>
             </ul>
-            <br></br>
+            <img className={styles.jump} src={h6} alt="shoshana and horse jumping hurdle" />
             <p className={styles.p}>And has an expansive breadth of experience from working in</p>
             <ul className={styles.ul}>
                 <li className={styles.li}>Foxhunting and Steeplechase </li>
@@ -45,7 +50,7 @@ function About() {
                 <li className={styles.li}>Trail guiding</li>
                 <li className={styles.li}>And much more!</li>
             </ul>
-            <img className={styles.cat} src={cat} alt="cat" />
+            <img className={styles.baby} src={h2} alt="baby horse on the farm" />
             <p className={styles.p}>When she’s not out working, Shoshana enjoys being with her husband, Jon, and her two children. She loves exploring the beaches of Maryland, opera (she is professionally trained!), making music, and giving back to the community. She is also a doula. </p>
             
         </div>
