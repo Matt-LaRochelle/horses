a490f8dea451df784ebc105f5808bb5ee98074eb
matthew <larochelle.matthew@gmail.com>
2023-04-07 00:15:44 -0700

    lesson headings tidy up

--- src/components/lessons/Lessons.jsx
+++ src/components/lessons/Lessons.jsx
@@ -13,19 +13,19 @@ function Lessons() {
             </div>
             <div className={styles.grid}>
                 <div className={styles.lesson}>
-                    <h3 className={styles.h3}>Private Introduction Lesson - Ages 12+ - One Time Lesson - 2 Hours - $120</h3>
+                    <h3 className={styles.h3}>Private Introduction Lesson</h3><h4>Ages 12+</h4><h4>One Time Lesson</h4><h4>2 Hours: $120</h4>
                     <p className={styles.p}>During our Private Introduction Lesson, you will meet one-on-one with our trainer, Shoshana Haag, and get a tour of the facilities, meet the horses, have a riding or driving evaluation, and discuss your visions for  your experience with Windmill Equestrian. Please ask lots of questions! After initial introductions, you will move on to grooming and tacking, learn fundamentals of horse care, and receive an intro to driving or horseback riding  lesson. Although targeted for potential riders newer to horses, who may need to “try it on”, this lesson is required for everyone  in order to be scheduled for any of the bundles. Waivers are required and anyone under age 16 must be accompanied by an adult for the entirety of the lesson. </p>
                 </div>
                 <div className={styles.lesson}>
-                    <h3 className={styles.h3}>Group Introduction Lesson - Ages 12+ - Four Rider Slots - 2.5 Hours - $60/Rider</h3>
+                    <h3 className={styles.h3}>Group Introduction Lesson</h3><h4>Ages 12+</h4><h4>Four Rider Slots</h4><h4>2.5 Hours: $60/Rider</h4>
                     <p className={styles.p}>Offered once a month, our Group Introduction Lesson is perfect for true beginners or those with little experience who want to make sure horseback riding is for them. During our 2.5 hour lesson, riders will be walked through the  basics of horse care, grooming, tacking, and one on one lesson time. Get a better understanding of the horse world, your needs and goals, and experience the wonder of horses! This lesson has zero commitment, but graduates of the group lesson are given priority for lesson booking. Waivers are required and anyone under age 16 must be accompanied by an adult for the entirety of the lesson.</p>
                 </div>
                 <div className={styles.lesson}>
-                    <h3 className={styles.h3}>Private One Hour Riding Lesson - Ages 12+ - All Levels - $75</h3>
+                    <h3 className={styles.h3}>Private One Hour Riding Lesson</h3><h4>Ages 12+</h4><h4>All Levels: $75</h4>
                     <p className={styles.p}>After completion of one of our Introduction Lessons, begin your journey with our trainer in private lessons. A combination of horse care and individualized time in the arena, the Private One Hour Lessons are tailored  to your specific needs and requests.  Anyone under age 16 must be accompanied by an adult for the entirety of the lesson. </p>
                 </div>
                 <div className={styles.lesson}>
-                    <h3 className={styles.h3}>Private One Hour Driving Lesson - Ages 12+ - Beginner- $95</h3>
+                    <h3 className={styles.h3}>Private One Hour Driving Lesson</h3><h4>Ages 12+</h4><h4>Beginner: $95</h4>
                     <p className={styles.p}>After completion of one of our Introduction Lessons, begin your journey with our trainer in private lessons. A combination of horse care and individualized time in the arena, the Private One Hour Lessons are tailored  to your specific needs and requests.  Anyone under age 18 must be accompanied by an adult for the entirety of the lesson. </p>
                 </div>
             </div>
