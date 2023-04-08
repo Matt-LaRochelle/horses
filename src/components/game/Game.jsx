f7fdde08c81a484005aa356a3561cd3ded361a48
matthew <larochelle.matthew@gmail.com>
2023-04-07 18:22:12 -0700

    jump and cactus working ok

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,22 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from './Game.module.css';
 
 function Game() {
-    const [jump, setJump] = useState(null);
 
-    function trexjump() {
-        setJump(styles.jump);
-        setTimeout(function () {
-            setJump(null);
-        }, 300);
+    const [tRexJump, setTRexJump] = useState(false)
+
+    function jump() {
+        if (tRexJump !== true) {
+            setTRexJump(true);
+            setTimeout(function () {
+                setTRexJump(false);
+            }, 1000);
+        }
     }
+
+
+    useEffect(() => {
+        document.addEventListener('keydown', detectKeyDown, true)
+    }, [])
+
+    const detectKeyDown = (e) => {
+        if (e.key === ' ') {
+            jump();
+        }
+    }
+
+    
 
     return (
         <div className={styles.game}>
-            <div className={  `head ${styles.dino} ${jump}`  }
-                 onKeyDown={(function (event) {
-                trexjump();
-                })}>
+            <div className= {tRexJump ? [styles.jump, styles.dino].join(' ') : [styles.dino]}>
 
             </div>
             <div className={styles.cactus}></div>
