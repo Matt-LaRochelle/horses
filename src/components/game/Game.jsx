be5919fc1565464c35b22e8288c2952a5c10ec9a
matthew <larochelle.matthew@gmail.com>
2023-04-14 14:37:07 -0700

    flappy bird style game adjusted for website

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,48 +1,144 @@
-import React, { useState, useEffect } from "react";
-import styles from './Game.module.css';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const BIRD_SIZE = 20;
+const GAME_WIDTH = 500;
+const GAME_HEIGHT = 500;
+const GRAVITY = 6;
+const JUMP_HEIGHT = 100;
+const OBSTACLE_WIDTH = 40;
+const OBSTACLE_GAP = 200;
+
+
 
 function Game() {
+  const [birdPosition, setBirdPosition] = useState(250);
+  const [gameHasStarted, setGameHasStarted] = useState(false);
+  const [obstacleHeight, setObstacleHeight] = useState(Math.floor(Math.random() * (GAME_HEIGHT - OBSTACLE_GAP)));;
+  const [obstacleLeft, setObstacleLeft] = useState(GAME_WIDTH - OBSTACLE_WIDTH);
+  const [score, setScore] = useState(0);
+  
+  const bottomObstacleHeight = GAME_HEIGHT - OBSTACLE_GAP - obstacleHeight;
 
-    const [tRexJump, setTRexJump] = useState(false)
-
-    function jump() {
-        if (tRexJump !== true) {
-            setTRexJump(true);
-            setTimeout(function () {
-                setTRexJump(false);
-            }, 1000);
-        }
+  useEffect(() => {
+    let timeId;
+    if ( gameHasStarted && birdPosition <  GAME_HEIGHT - BIRD_SIZE) {
+      timeId = setInterval(() => {
+        setBirdPosition((birdPosition) => birdPosition + GRAVITY);
+      }, 24);
     }
 
+    return () => {
+      clearInterval(timeId);
+    };
+  }, [birdPosition, gameHasStarted]);
 
-    useEffect(() => {
-        document.addEventListener('keydown', detectKeyDown, true)
-    }, [])
+  useEffect(() => {
+    let obstacleId;
+    if (gameHasStarted && obstacleLeft >= -OBSTACLE_WIDTH) {
+      obstacleId = setInterval(() => {
+        setObstacleLeft((obstacleLeft) => obstacleLeft - 5);
+      }, 24);
 
-    const detectKeyDown = (e) => {
-        if (e.key === ' ') {
-            jump();
-        }
+      return () => {
+        clearInterval(obstacleId);
+      };
     }
+    else if (gameHasStarted) {
+      setObstacleLeft(GAME_WIDTH - OBSTACLE_WIDTH);
+      setObstacleHeight(Math.floor(Math.random() * (GAME_HEIGHT - OBSTACLE_GAP)));
+      setScore((score) => score + 1);
+    }
+  }, [gameHasStarted, obstacleLeft]);
 
-    useEffect(() => {
-        const isAlive = setInterval(() => {
-            let dinoTop = window.getComputedStyle(styles.dino).getPropertyValue("top");
-            console.log(dinoTop);
-        }, 10);
-        return () => clearInterval(isAlive);
-    }, []);
+  useEffect(() => {
+    const hasCollidedWithTopObstacle = 
+      birdPosition >= 0 && birdPosition < obstacleHeight;
+    const hasCollidedWithBottomObstacle = 
+      birdPosition <= 500 && birdPosition >= 500 - bottomObstacleHeight;
 
-    
+    if (obstacleLeft >= 0 && obstacleLeft <= OBSTACLE_WIDTH && (hasCollidedWithTopObstacle || hasCollidedWithBottomObstacle)) {
+      setGameHasStarted(false);
+      setBirdPosition(250);
+      setObstacleHeight(Math.floor(Math.random() * (GAME_HEIGHT - OBSTACLE_GAP)));
+      setObstacleLeft(GAME_WIDTH - OBSTACLE_WIDTH);
+    }
+  }, [birdPosition, obstacleHeight, bottomObstacleHeight, obstacleLeft]);
 
-    return (
-        <div className={styles.game}>
-            <div className= {tRexJump ? [styles.jump, styles.dino].join(' ') : [styles.dino]}>
+  const handleClick = () => {
+    let newBirdPosition = birdPosition - JUMP_HEIGHT;
+    if (!gameHasStarted) {
+      setGameHasStarted(true);
+      setScore(0);
+    }
+    else if (newBirdPosition < 0) {
+      setBirdPosition(0);
+    } else {
+    setBirdPosition(newBirdPosition);
+    }
+  };
 
-            </div>
-            <div className={styles.cactus}></div>
-        </div>
-    );
+
+  return (
+    <Div onClick={handleClick}>
+      <GameBox height={GAME_HEIGHT} width={GAME_WIDTH}>
+        <Obstacle 
+          top={0}
+          width={OBSTACLE_WIDTH}
+          height={obstacleHeight}
+          left={obstacleLeft}
+        />
+        <Obstacle 
+          top={GAME_HEIGHT - (obstacleHeight + bottomObstacleHeight)}
+          width={OBSTACLE_WIDTH}
+          height={bottomObstacleHeight}
+          left={obstacleLeft}
+        />
+        <Bird size={BIRD_SIZE} top={birdPosition} />
+      </GameBox>
+      <span> {score} </span>
+    </Div>
+  );
 }
 
 export default Game;
+
+
+
+
+const Bird = styled.div`
+position: absolute;
+background-color: red;
+height: ${(props) => props.size}px;
+width: ${(props) => props.size}px;
+top: ${(props) => props.top}px;
+border-radius: 50%;
+`;
+
+const Div = styled.div`
+display: flex;
+width: 100%;
+justify-content: center;
+& span{
+  color: white;
+  font-size: 24px;
+  position: absolute;
+}
+`;
+
+const GameBox = styled.div`
+height: ${(props) => props.height}px;
+width: ${(props) => props.height}px;
+background-color: blue;
+overflow: hidden;
+position: relative;
+`;
+
+const Obstacle = styled.div`
+  position: relative;
+  top: ${(props) => props.top}px;
+  background-color: green;
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  left: ${(props) => props.left}px;
+`;
